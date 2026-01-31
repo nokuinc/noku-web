e58127b81470630c20fe6852a3dd122b50830933
NoKu <nokuinc@outlook.com>
2026-01-30 22:14:57 -0800

    git push origin main

--- components/elements/Preloader.js
+++ components/elements/Preloader.js
@@ -5,13 +5,26 @@ const Preloader = () => {
         <>
             <div id="preloader-active">
                 <div className="preloader flex-1 content-center">
-                    <div className="logo flex items-center justify-center h-screen w-screen jump">
+                    <div className="logo flex flex-col items-center justify-center h-screen w-screen jump">
                         <img
                             src="/assets/imgs/logos/noku-icon.svg"
                             alt="NoKu"
                             className="mx-auto"
-                            style={{ width: 320, maxWidth: "86vw", height: "auto" }}
+                            style={{ width: 224, maxWidth: "60vw", height: "auto" }}
                         />
+                        <div
+                            style={{
+                                marginTop: 18,
+                                fontFamily: "Montserrat, Arial, sans-serif",
+                                fontSize: 48,
+                                fontWeight: 800,
+                                letterSpacing: 2,
+                                lineHeight: 1,
+                                color: "#1E2A5A",
+                            }}
+                        >
+                            NOKU(诺库)
+                        </div>
                     </div>
                 </div>
             </div>
